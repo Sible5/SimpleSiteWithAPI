1a6764753dc3f1e7f7ef10c4d31be1d3b6e56fea
Sible5 <39971980+Sible5@users.noreply.github.com>
2021-08-16 13:04:44 -0500

    Add files via upload

---
+++ WeatherWebsite/public_html/weather.js
@@ -0,0 +1,28 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+"use strict"
+
+
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Ankeny&appid=a1484c058a6d7d94fc3699dafaa02c92";
+
+$.ajax ({
+    url: url, 
+    success: function (result){
+        console.log(result);
+        console.log(result.name);
+        
+        $("#location").text(result.name);
+        
+        $("#sky").text(result.weather[0].description);
+
+        let F = Math.round(result.main.temp * (9/5) - 459.67);
+        $("#temperature").text(F);
+        let windSpeed = Math.round(result.wind.speed / .44704);
+
+        $("#wind").text(windSpeed);
+    }
+})
+
